552f14888f578caf053da04ada2d01ad18512d6d
eco-mathew <dmsckddh@gmail.com>
2021-09-28 15:18:24 +0900

    setup folder update

---
+++ blockoli_setup/blockoli_v1.0.1/blockoli_v1.0.1/arduexec/resources/electron/browser/api/session.js
@@ -0,0 +1,22 @@
+const {EventEmitter} = require('events')
+const {app} = require('electron')
+const {fromPartition, _setWrapSession} = process.atomBinding('session')
+
+// Returns the default session.
+Object.defineProperties(exports, {
+  defaultSession: {
+    enumerable: true,
+    get () { return fromPartition('') }
+  },
+  fromPartition: {
+    enumerable: true,
+    value: fromPartition
+  }
+})
+
+// Wraps native Session class.
+_setWrapSession(function (session) {
+  // Session is an EventEmitter.
+  Object.setPrototypeOf(session, EventEmitter.prototype)
+  app.emit('session-created', session)
+})
